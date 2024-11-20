c8e15a75e37ab9a30004529d891e546b6c9f8d27
Or Ben Naim <orbennaim123@gmail.com>
2024-11-20 13:41:04 +0200

    .

--- Tic-Tac-Toe/src/App.tsx
+++ Tic-Tac-Toe/src/App.tsx
@@ -1,7 +1,40 @@
-export default function Square() {
+import { useState } from "react";
+
+// interface SquareProps{
+//   value: string;
+// }
+
+function Square(){
+  const [value, setValue] = useState("")
+
+  function handleClick(){
+    setValue('X');
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
+}
+
+
+export default function Board() {
   return (
     <>
-      <button className="square">X</button><button className="square">X</button>;
+      <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      </div>
+      <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      </div>
+      <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+      </div>
     </>
   );
 }
